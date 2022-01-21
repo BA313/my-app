67a6b9727bfdfe5510f0390ec1ab82f300e9f3fc
Ben <benaldrich2000@gmail.com>
2022-01-20 20:08:53 -0800

    food stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { AmplifyProvider, SelectField, Card, useTheme } from '@aws-amplify/ui-react';
-import { useState } from 'react';
 
 import { theme } from './theme';
 
-import React from 'react';
+import React, {useState}  from 'react';
 import { Amplify } from 'aws-amplify';
 
 import { Authenticator, TextField} from '@aws-amplify/ui-react';
@@ -13,28 +12,49 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 export default function App() {
+
+  const [defCal, cal2] = useState({num: 0});
+
+  var cals
+  const setValue = (e) => {
+    console.log(e);
+    switch(e){
+      case "item1":
+        cal2({num: 530});
+        console.log("BIG MAC");
+        break;
+      case "item2":
+        cal2({num: 190});
+        console.log("Chicken McNuggets® (4 piece)");
+        break;
+      case "item3":
+        cal2({num: 510});
+        console.log("Large French Fries");
+        break;
+      default:
+        cal2({num: 0});
+        console.log("DEFAULT");
+        break;
+    }
+  }
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
+    <Card>
           <SelectField
-            label="Fruit"
-            labelHidden={true}
+            label="McFood"
             placeholder="Please select a food"
-            //onChange={(e) => setValue(e.target.value)}
+            onChange={(e) => setValue(e.target.value)}
           >
-            <option value="apple">Apple</option>
-            <option value="banana">Banana</option>
-            <option value="orange">Orange</option>
-          </SelectField>;
+            <option value="item1">Big Mac</option>
+            <option value="item2">Chicken McNuggets® (4 piece)</option>
+            <option value="item3">Large French Fries</option>
+          </SelectField>
           <TextField
-              label="Calories"
+              label="McCalories"
               placeholder="resulting calories"
-            />;
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+              value={defCal.num}
+              onChange={(e) => cal2({num: e.target.value})}
+            />
+      </Card>
   );
 }
