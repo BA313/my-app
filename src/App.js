57bffe57d9b097b35c9fbb9f86727c02743b3165
Ben <benaldrich2000@gmail.com>
2022-01-20 18:54:07 -0800

    Revert "v2"
    This reverts commit 65b53ee99fff47220661d03f9633aa90705cf700.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import { AmplifyProvider, SelectField, Card, useTheme } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import { theme } from './theme';
-
-import React from 'react';
-import { Amplify } from 'aws-amplify';
+import logo from './logo.svg';
+import './App.css';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -14,13 +9,22 @@ Amplify.configure(awsExports);
 
 export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
           <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
