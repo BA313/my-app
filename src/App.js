99746ddf537c03069d232d9d12df4f7f1852a0dd
Ben <benaldrich2000@gmail.com>
2022-01-20 18:59:33 -0800

    Auto stash before revert of "v2"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { AmplifyProvider, SelectField, Card, useTheme } from '@aws-amplify/ui-react';
+import { useState } from 'react';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { theme } from './theme';
+
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator, TextField} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
@@ -9,22 +14,27 @@ Amplify.configure(awsExports);
 
 export default function App() {
   return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
           <h1>Hello {user.username}</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <SelectField
+            label="Fruit"
+            labelHidden={true}
+            placeholder="Please select a food"
+            //onChange={(e) => setValue(e.target.value)}
+          >
+            <option value="apple">Apple</option>
+            <option value="banana">Banana</option>
+            <option value="orange">Orange</option>
+          </SelectField>;
+          <TextField
+              label="Calories"
+              placeholder="resulting calories"
+            />;
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
